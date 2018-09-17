f5d7cc7ab2543652e45aa8c8796cfb038ae967c5
Robert Pustułka <robert.pustulka@gmail.com>
2018-09-17 14:50:33 +0200

    Port resolver

---
+++ src/Identifier/Resolver/OrmResolver.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+namespace Phautehntic\Authentication\Identifier\Resolver;
+
+use ArrayAccess;
+use Authentication\Identifier\Resolver\ResolverInterface;
+use Cake\Core\InstanceConfigTrait;
+use Cake\ORM\Locator\LocatorAwareTrait;
+
+class OrmResolver implements ResolverInterface
+{
+
+    use InstanceConfigTrait;
+    use LocatorAwareTrait;
+
+    /**
+     * Default configuration.
+     * - `userModel` The alias for users table, defaults to Users.
+     * - `finder` The finder method to use to fetch user record. Defaults to 'all'.
+     *   You can set finder name as string or an array where key is finder name and value
+     *   is an array passed to `Table::find()` options.
+     *   E.g. ['finderName' => ['some_finder_option' => 'some_value']]
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'userModel' => 'Users',
+        'finder' => 'all',
+    ];
+
+    protected $_config;
+
+    /**
+     * Constructor.
+     *
+     * @param array $config Config array.
+     */
+    public function __construct(array $config = [])
+    {
+        $this->setConfig($config);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function find(array $conditions): ?ArrayAccess
+    {
+        $table = $this->getTableLocator()->get($this->getConfig('userModel'));
+
+        $query = $table->query();
+        $finders = (array)$this->getConfig('finder');
+        foreach ($finders as $finder => $options) {
+            if (is_string($options)) {
+                $query->find($options);
+            } else {
+                $query->find($finder, $options);
+            }
+        }
+
+        $where = [];
+        foreach ($conditions as $field => $value) {
+            $field = $table->aliasField($field);
+            if (is_array($value)) {
+                $field = $field . ' IN';
+            }
+            $where[$field] = $value;
+        }
+
+        return $query->where($where)->first();
+    }
+}
---
+++ tests/TestCase/Identifier/Resolver/OrmResolverTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace Phautehntic\Authentication\Test\TestCase\Identifier\Resolver;
+
+use Cake\Datasource\EntityInterface;
+use Phautehntic\Authentication\Identifier\Resolver\OrmResolver;
+use Phautehntic\Authentication\Test\TestCase\AuthenticationTestCase;
+
+class OrmResolverTest extends AuthenticationTestCase
+{
+    public function testFindDefault()
+    {
+        $resolver = new OrmResolver();
+
+        $user = $resolver->find([
+            'username' => 'mariano'
+        ]);
+
+        $this->assertInstanceOf(EntityInterface::class, $user);
+        $this->assertEquals('mariano', $user['username']);
+    }
+
+    public function testFindConfig()
+    {
+        $resolver = new OrmResolver([
+            'userModel' => 'AuthUsers',
+            'finder' => [
+                'all',
+                'auth' => ['return_created' => true]
+            ]
+        ]);
+
+        $user = $resolver->find([
+            'username' => 'mariano'
+        ]);
+
+        $this->assertNotEmpty($user->created);
+    }
+
+    public function testFindAnd()
+    {
+        $resolver = new OrmResolver();
+
+        $user = $resolver->find([
+            'id' => 1,
+            'username' => 'mariano'
+        ]);
+
+        $this->assertEquals(1, $user['id']);
+    }
+
+    public function testFindMissing()
+    {
+        $resolver = new OrmResolver();
+
+        $user = $resolver->find([
+            'id' => 1,
+            'username' => 'luigiano'
+        ]);
+
+        $this->assertNull($user);
+    }
+
+    public function testFindMultipleValues()
+    {
+        $resolver = new OrmResolver();
+
+        $user = $resolver->find([
+            'username' => [
+                'luigiano',
+                'mariano'
+            ]
+        ]);
+
+        $this->assertEquals(1, $user['id']);
+    }
+}
